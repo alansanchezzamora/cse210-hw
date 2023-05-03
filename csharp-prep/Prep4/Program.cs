35d20ffb9c4c82f0ec4b387156ef51ecceab07a5
Alan Sánchez <alan.sanchez.zamor@gmail.com>
2023-05-03 16:27:13 -0600

    Activity Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        
+        int newNumber = -1;
+
+        while (newNumber != 0) 
+        {
+            Console.Write("Enter number: ");
+            newNumber = int.Parse(Console.ReadLine());
+            numbers.Add(newNumber);
+        }
+
+        int suma = 0;
+
+        foreach (int num in numbers)
+        {
+            suma += num ;
+        }
+        Console.WriteLine($"The sum is {suma}");
+
+        float average = 0;
+
+        average = suma / (numbers.Count-1);
+        Console.WriteLine($"The average is {average}");
+
+        Console.WriteLine($"The largest number is {numbers.Max()}");
     }
 }
