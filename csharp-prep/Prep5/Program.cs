e61e52321fef62f29f778f0b7de6f7fc7fb1e293
Alan Sánchez <alan.sanchez.zamor@gmail.com>
2023-05-03 21:50:23 -0600

    Activity Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+
+        string userName = GetName();
+        int userNumber = GetNumber();
+
+        int squared = SquareNumber(userNumber);
+
+        PrintResult(userName, squared);
+
+        static void Welcome()
+        {
+            Console.WriteLine("Welcome to the program");
+        }
+        static string GetName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        
+        static int GetNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+        
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+        static void PrintResult(string name, int square)
+        {
+            Console.Write($"{name}, the square of your number is {square}");
+        }
     }
 }
